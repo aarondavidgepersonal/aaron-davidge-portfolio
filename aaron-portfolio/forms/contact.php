adaef2addeb1ebe98e2c89f608d922ce7e4a644c
aarondavidgepersonal <114311538+aarondavidgepersonal@users.noreply.github.com>
2022-09-29 16:24:27 -0500

    new
    contact

--- aaron-portfolio/forms/contact.php
+++ aaron-portfolio/forms/contact.php
@@ -1,22 +1,5 @@
 <?php
 
-
-$name = $POST['name'];
-$email = $POST['email'];
-$subject = $POST['subject'];
-$message = $POST['message'];
-
-
-$mailheader = "From:".$name."<".$email.">\r\n";
-
-$recipient = "aarondavidgejob@gmail.com";
-
-mail($recipient,$subject,$message,$mailheader)
-or die("Error");
-
-echo'Message Sent';
-
-
-
+echo'Hello World';
 
 ?>
